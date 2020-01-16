cca08eedaa5a69023c89798abcf3a16f77ef172a
Ying Ting <tingting940201@gmail.com>
2020-01-16 23:33:48 +0800

    add Taiwan

---
+++ driving.py
@@ -0,0 +1,8 @@
+country = input('please input your country:')
+age = input('please input your age:')
+age = int(age)
+if country == 'taiwan':
+    if age >= 18:
+    	print('you can drive')
+    else:
+    	print('you cannot drive')
